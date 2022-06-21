d45de1fd9da20c605986a3c25eb30dbd13fbc58e
valencejas <107890215+valencejas@users.noreply.github.com>
2022-06-21 12:56:20 -0300

    logo

--- proyectofinal/src/app/app.module.ts
+++ proyectofinal/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogoapComponent } from './componets/logoap/logoap.component';
+import { HeaderComponent } from './componets/header/header.component';
+import { RedesComponent } from './componets/redes/redes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoapComponent,
+    HeaderComponent,
+    RedesComponent
   ],
   imports: [
     BrowserModule,
